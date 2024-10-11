45d6f3f50ea4377b63aca94d9cc648b2cbb33bf6
bhukuma <43422209+bhukuma@users.noreply.github.com>
2024-10-11 13:26:28 +0530

    Update main.tf

--- thing/main.tf
+++ thing/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "esp32datatos3"        # Change this to your S3 bucket
-    key            = "terraform/thing/terraform.tfstate"  # Path within the bucket to store the state file
-    region         = "us-east-1"                  # Change this to your AWS region
-    encrypt        = true                         # Enable server-side encryption of the state file
-  }
-}
-
-
 provider "aws" {
   region = "us-east-1"  # Specify your preferred AWS region
 }
