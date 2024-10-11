7770b27dc9f65e1500750f512e005f4fc431a236
bhukuma <43422209+bhukuma@users.noreply.github.com>
2024-10-12 02:25:48 +0530

    Update main.tf

--- policies/main.tf
+++ policies/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "esp32datatos3"        # Change this to your S3 bucket
+    key            = "terraform/policies/terraform.tfstate"  # Path within the bucket to store the state file
+    region         = "us-east-1"                  # Change this to your AWS region
+    encrypt        = true                         # Enable server-side encryption of the state file
+  }
+}
+
 resource "aws_iot_policy" "my_iot_policy" {
   name = "IoTThingAndManagementPolicy"
 
