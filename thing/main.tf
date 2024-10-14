2d4f441274dd618dc49e76947638e567bcd0d397
bhukuma <43422209+bhukuma@users.noreply.github.com>
2024-10-14 18:34:35 +0530

    Update main.tf

--- thing/main.tf
+++ thing/main.tf
@@ -40,13 +40,33 @@ resource "aws_iot_thing_principal_attachment" "thing_cert_attachment" {
   principal = aws_iot_certificate.example_certificate.arn
 }
 
-# Output the private key and certificate for later use
-output "private_key_pem" {
-  value = tls_private_key.example_key.private_key_pem
-  sensitive = true
+# Create a Secret in AWS Secrets Manager for the private key
+resource "aws_secretsmanager_secret" "private_key_secret" {
+  name        = "iot-private-key"
+  description = "Private key for IoT Thing"
 }
 
-output "certificate_pem" {
-  value = aws_iot_certificate.example_certificate.certificate_pem
-  sensitive = true
+resource "aws_secretsmanager_secret_version" "private_key_version" {
+  secret_id     = aws_secretsmanager_secret.private_key_secret.id
+  secret_string = tls_private_key.example_key.private_key_pem
+}
+
+# Create a Secret in AWS Secrets Manager for the certificate
+resource "aws_secretsmanager_secret" "certificate_secret" {
+  name        = "iot-certificate"
+  description = "Certificate for IoT Thing"
+}
+
+resource "aws_secretsmanager_secret_version" "certificate_version" {
+  secret_id     = aws_secretsmanager_secret.certificate_secret.id
+  secret_string = aws_iot_certificate.example_certificate.certificate_pem
+}
+
+# Output the ARN of the secrets for reference
+output "private_key_secret_arn" {
+  value = aws_secretsmanager_secret.private_key_secret.arn
+}
+
+output "certificate_secret_arn" {
+  value = aws_secretsmanager_secret.certificate_secret.arn
 }
